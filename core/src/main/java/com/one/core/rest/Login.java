6f9f7a629733eb0c5ac0fc115d82ac3342e0ec4a
ravila2020 <ravila@oneproject.com>
2020-10-20 18:23:33 -0500

    Instalacion base del core

---
+++ core/src/main/java/com/one/core/rest/Login.java
@@ -0,0 +1,16 @@
+package com.one.core.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class Login {
+
+	@GetMapping
+	public String listar(){
+		String Saludo = "{\"Mensaje\":\"Hola\"}";
+		return Saludo;
+	}
+}
